99582b244a977770c454ef8f0ab92786b9b798cd
MasterMind4-0 <119766429+MasterMind4-0@users.noreply.github.com>
2024-09-17 18:57:49 -0400

    File dir changes, adding features, fixing code
    Added days, will be days system will be built later.
    Added weapons
    Attempted to fix the traveling merchant shop

--- GAME/main.py
+++ GAME/main.py
@@ -1,17 +1,29 @@
-from os import stat
 import random
 import sys
 import time
 import json
 
 to_be_saved = {
+  'days': 0,
   "health": 20,
   "max_health": 20,
   "cclass": None,
   "name": None,
   'stats': {'strength': 1, 'dexterity': 1, 'charisma': 1, 'intelligence': 1, 'defense': 1, 'wisdom': 1},
-  'inventory': []  
+  'inventory': [],
+  'coin': 0
 }
+
+weapons = {
+  "Mace": 2,
+  "Shortbow": 2,
+  "Longbow": 4
+  
+}
+
+def random_event_picker():
+  events = [tavern, travling_merchant]
+  random.choice(events)()
 
 def start():
   global to_be_saved
@@ -19,17 +31,16 @@ def start():
   time.sleep(.5)
   print("To begin the game, we first must create a character, if you have already created one, simply enter that character's name.")
   time.sleep(1)
-  to_be_saved['name'] = input("What will be your character's name?\n")
+  to_be_saved['name'] = input("What will be your character's name?\n").lower()
   print('Select your class below:\n')
   to_be_saved['cclass'] = input('''
-        Knight - Press K to Select
-        - Iron Armor - +.2 Defense
-        - Mace - +2 Attack
-        - Shield - +.1 Defense
-        - Max Health - +2
-        - Strength - +.2
-        - 15 Coins
-        \n
+Knight - Press K to Select
+  - Iron Armor - +.2 Defense
+  - Mace - +2 Attack
+  - Shield - +.1 Defense
+  - Max Health - +2
+  - Strength - +.2
+  - 15 Coins\n
         ''')
 
   if to_be_saved['cclass'].lower() == 'k':
@@ -40,15 +51,11 @@ def start():
     to_be_saved['stats']['defense'] += .3
     print('You have chosen the Knight!')
     print(to_be_saved)
-    with open(f'{to_be_saved["name"]}.json', 'w') as file:
+    with open(f'GAME\\SAVES\\{to_be_saved["name"]}.json', 'w') as file:
       json.dump(to_be_saved, file)
 
   print('Now that you have your character... Let us begin!')
   random_event_picker()
-
-def random_event_picker():
-  events = [(tavern, travling_merchant)]
-  random.choice(events)
 
 def tavern():
   print('You entered tavern.')
@@ -66,5 +73,28 @@ def travling_merchant():
   else:
     if to_be_saved['stats']['wisdom'] >= 2:
       print("You see the merchant's smile fade subtly as you continue your travels.")
-      
+
+def merchant_shop():
+  item1 = random.choice(weapons)
+  item2 = random.choice(weapons)
+  item3 = random.choice(weapons)
+  while 1 > 0:
+    print('1. Buy items')
+    print('2. Sell items')
+    print('3. Leave')
+    t = input('')
+    if t == '1':
+      print(f'''
+Merchant's shop
+            
+1. {item1}
+2. {item2}
+3. {item3}
+
+
+Coin: {to_be_saved['coin']}
+
+Inventory: {to_be_saved["inventory"]}\n
+''')
+
 start()
